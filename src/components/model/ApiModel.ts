a63b55229acced2cf1da911e18d0b4cf037b3ebb
Lena_Menson <8000438@mail.ru>
2025-02-08 12:33:33 +0300

    первая итерация 8 проектная работа. Архитектура.

---
+++ src/components/model/ApiModel.ts
@@ -0,0 +1,10 @@
+import { ApiListResponse, Api } from '../base/api'
+
+
+export interface IApiModel {}
+export class ApiModel extends Api {
+
+    // получение массива карточек
+
+    // получение ответа от сервера по заказу
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import './scss/styles.scss';
+
+
+
+
+const cardCatalogTemplate = document.querySelector('#card-catalog') as HTMLTemplateElement;
+const cardPreviewTemplate = document.querySelector('#card-preview') as HTMLTemplateElement;
+const basketTemplate = document.querySelector('#basket') as HTMLTemplateElement;
+const cardBasketTemplate = document.querySelector('#card-basket') as HTMLTemplateElement;
+const orderTemplate = document.querySelector('#order') as HTMLTemplateElement;
+const contactsTemplate = document.querySelector('#contacts') as HTMLTemplateElement;
+const successTemplate = document.querySelector('#success') as HTMLTemplateElement;
